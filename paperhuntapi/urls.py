7071838b32337bcf3ce9d5e1c527bb92afd60a22
paul meyers <pmkundika@gmail.com>
2025-12-21 11:22:05 +0200

    changed users route to auth

--- paperhuntapi/urls.py
+++ paperhuntapi/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/users/', include('users.urls')),
+    path('api/v1/auth/', include('users.urls')),
     path('api/v1/papers/', include('papers.urls'), name='papers'),
     path('api/v1/ai_chat/', include('ai_chats.urls'), name='ai_chats'),
 ]
